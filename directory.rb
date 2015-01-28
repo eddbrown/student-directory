13a66236c0a2f7f38526a7cc55ba866d33cf9f00
Edward Brown <edward.brown.london@gmail.com>
2015-01-28 18:47:13 +0000

    Exercise 1 completed

--- directory.rb
+++ directory.rb
@@ -1,35 +1,33 @@
 #inputting students into an array
 
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students\nTo finish, just hit return twice\n"
 	students = []
 	name = gets.chomp
 	while !name.empty? do
 		students << {:name => name, :cohort => :february}
-		puts "Now we have #{students.length} students"
+		print "Now we have #{students.length} students\n"
 		name = gets.chomp
 	end
 	return students
 end
 
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-------------"
+	print "The students of my cohort at Makers Academy\n-------------\n"
 end
 
-def print(students)
+def print_list(students)
 	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+		print "#{student[:name]} (#{student[:cohort]} cohort)\n"
 	end
 end
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+	print "Overall, we have #{names.length} great students\n"
 end
 
 students = input_students
 print_header
-print(students)
+print_list(students)
 print_footer(students)
 
