85d8eb0ecb8d0d3be73a1906fa17f9a19dfc816b
Edward Brown <edward.brown.london@gmail.com>
2015-01-28 18:08:42 +0000

    Added comments to directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -8,7 +9,8 @@ puts "Alex de Large"
 puts "The Alien"
 puts "Freddy Kruger"
 puts "The Joker"
-
+#finally, we print the total
 print "Overall, we have "
+#note that print does not add an extra line after outputting the text
 print 8
 puts " great students"
