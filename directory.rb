69939b8fe68e2acbab3e885a1461534e2c94d47e
Edward Brown <edward.brown.london@gmail.com>
2015-01-28 18:41:55 +0000

    Introduced user inputs for student names

--- directory.rb
+++ directory.rb
@@ -1,24 +1,26 @@
 #inputting students into an array
-students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex de Large",
-"The Alien",
-"Freddy Kruger",
-"Terminator",
-"The Joker",
-]
+
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	students = []
+	name = gets.chomp
+	while !name.empty? do
+		students << {:name => name, :cohort => :february}
+		puts "Now we have #{students.length} students"
+		name = gets.chomp
+	end
+	return students
+end
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "-------------"
 end
 
-def print(names)
-	names.each do |name|
-		puts name
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
@@ -26,6 +28,7 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
