01cce760d8014b1188d32465d400faadfe46cf39
Edward Brown <edward.brown.london@gmail.com>
2015-01-28 18:18:50 +0000

    Entered student names into array and tidied code

--- directory.rb
+++ directory.rb
@@ -1,15 +1,19 @@
-student_count = 9
-#first we print the list of students
+#inputting students into an array
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex de Large",
+"The Alien",
+"Freddy Kruger",
+"Terminator",
+"The Joker",
+]
+#print the array
 puts "The students of my cohort at Makers Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex de Large"
-puts "The Alien"
-puts "Freddy Kruger"
-puts "Terminator"
-puts "The Joker"
-#finally, we print the total
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+	puts student
+end
+puts "Overall, we have #{students.length} great students"
